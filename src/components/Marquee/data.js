b37dcd9c37982c6a8c646e560fd179272ed13b57
Woong Yu <juun.yuu@gmail.com>
2026-02-18 17:50:14 +0900

    Add marquee data for projects alpha, beta, and gamma

---
+++ src/components/Marquee/data.js
@@ -0,0 +1,70 @@
+export const marqueeData = {
+    "project-alpha": [
+        {
+            src: "https://picsum.photos/1920/1080?random=1",
+            alt: "Alpha – Scene 01",
+        },
+        {
+            src: "https://picsum.photos/1920/1080?random=2",
+            alt: "Alpha – Scene 02",
+        },
+        {
+            src: "https://picsum.photos/1920/1080?random=3",
+            alt: "Alpha – Scene 03",
+        },
+        {
+            src: "https://picsum.photos/1920/1080?random=4",
+            alt: "Alpha – Scene 04",
+        },
+        {
+            src: "https://picsum.photos/1920/1080?random=5",
+            alt: "Alpha – Scene 05",
+        },
+    ],
+
+    "project-beta": [
+        {
+            src: "https://picsum.photos/1920/1080?random=1",
+            alt: "Beta – Scene 01",
+        },
+        {
+            src: "https://picsum.photos/1920/1080?random=2",
+            alt: "Beta – Scene 02",
+        },
+        {
+            src: "https://picsum.photos/1920/1080?random=3",
+            alt: "Beta – Scene 03",
+        },
+        {
+            src: "https://picsum.photos/1920/1080?random=4",
+            alt: "Beta – Scene 04",
+        },
+        {
+            src: "https://picsum.photos/1920/1080?random=5",
+            alt: "Beta – Scene 05",
+        },
+    ],
+
+    "project-gamma": [
+        {
+            src: "https://picsum.photos/1920/1080?random=1",
+            alt: "Gamma – Scene 01",
+        },
+        {
+            src: "https://picsum.photos/1920/1080?random=2",
+            alt: "Gamma – Scene 02",
+        },
+        {
+            src: "https://picsum.photos/1920/1080?random=3",
+            alt: "Gamma – Scene 03",
+        },
+        {
+            src: "https://picsum.photos/1920/1080?random=4",
+            alt: "Gamma – Scene 04",
+        },
+        {
+            src: "https://picsum.photos/1920/1080?random=5",
+            alt: "Gamma – Scene 05",
+        },
+    ],
+}
