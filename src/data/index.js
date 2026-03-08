72acf4dc2f6945c182f90fd5abbac363d5422564
Woong Yu <juun.yuu@gmail.com>
2026-03-08 11:08:09 +0900

    Create index.js

---
+++ src/data/index.js
@@ -0,0 +1,23 @@
+// Slugs / Paths
+export const HOME_SLUG = 'home'
+export const FOUR_OH_FOUR_SLUG = '404'
+export const PRODUCTS_SLUG = 'products'
+
+// Doc Types
+export const DOC_TYPES = {
+  PAGE: 'page',
+  PRODUCT: 'product',
+}
+
+// Transitions
+export const TRANSITION_DURATION = 1.2
+// export const TRANSITION_DURATION = 40.2
+export const TRANSITION_DURATION_MS = TRANSITION_DURATION * 1000
+
+// Product Grid Views
+export const PRODUCT_GRID_VIEW_TYPES = {
+  DEFAULT: 'DEFAULT',
+  COLUMN: 'COLUMN',
+  SMALL: 'SMALL',
+  TEXT: 'TEXT',
+}
