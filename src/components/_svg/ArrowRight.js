716c65bb14e616b555037ca37fc14f995a64a89e
Woong Yu <juun.yuu@gmail.com>
2026-03-27 13:26:01 +0900

    Update and rename ArrowRight.js to ArrowRight.js

---
+++ src/components/_svg/ArrowRight.js
@@ -0,0 +1,18 @@
+const ArrowDown = ({ className }) => {
+    return (
+        <svg
+            className={className}
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M7 1V13M7 13L1 7M7 13L13 7"
+                fill="currentColor"
+                data-themed="color, fill"
+            />
+        </svg>
+    )
+}
+
+export default ArrowDown
